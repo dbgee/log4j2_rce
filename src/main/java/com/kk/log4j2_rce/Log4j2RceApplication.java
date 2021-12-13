f0ffc4f3c93c8ac6b11362d93c9a1fd554401827
dbgee <46910972+dbgee@users.noreply.github.com>
2021-12-13 14:43:17 +0800

    add rmi trustURLCodebase

--- src/main/java/com/kk/log4j2_rce/Log4j2RceApplication.java
+++ src/main/java/com/kk/log4j2_rce/Log4j2RceApplication.java
@@ -8,7 +8,9 @@ public class Log4j2RceApplication {
 
     public static void main(String[] args) {
         //高版本的jdk默认trustURLCodebase为false不能成功利用JNDI注入,需要设置为true才能复现
-        System.setProperty("com.sun.jndi.ldap.object.trustURLCodebase","true");
+        //如果复现失败，大概率是java 版本或log4j 版本问题，可以取消下面的两条注释，再进行尝试
+//        System.setProperty("com.sun.jndi.ldap.object.trustURLCodebase","true");
+//        System.setProperty("com.sun.jndi.rmi.object.trustURLCodebase","true");
         SpringApplication.run(Log4j2RceApplication.class, args);
     }
 
